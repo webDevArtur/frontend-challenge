70d18d782742007323a1ed25de2db6acb6896571
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2024-01-30 04:06:08 +0300

    Update AppRouter.tsx

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,22 +1,30 @@
-import {Route, Routes} from 'react-router-dom';
-import {Outlet} from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Layout from '../components/Layout/Layout.tsx';
 import AllCatsPage from "../pages/AllCatsPage/AllCatsPage.tsx";
 import LovelyCatsPage from "../pages/LovelyCatsPage/LovelyCatsPage.tsx";
 
-
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/" element={
+            <Route
+                path="/"
+                element={
                     <Layout>
-                        <Outlet/>
+                        <Outlet />
                     </Layout>
-            }>
-                <Route index element={<AllCatsPage />} />
-
-                <Route path="/allcats" element={<AllCatsPage/>} />
-                <Route path="/lovelycats" element={<LovelyCatsPage/>}/>
+                }
+            >
+                <Route
+                    index
+                    element={
+                        <>
+                            <Navigate to="/allcats" />
+                        </>
+                    }
+                />
+                <Route path="/allcats" element={<AllCatsPage />} />
+                <Route path="/lovelycats" element={<LovelyCatsPage />} />
             </Route>
         </Routes>
     );
