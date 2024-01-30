1e7bca1216d3aaf2c9fdf661de988c377e913366
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2024-01-30 04:01:02 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,43 @@
-import { Link } from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 
 function Header() {
-
+    const location = useLocation();
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{flexGrow: 1}}>
             <AppBar position="static">
                 <Toolbar>
-                    <Link to="/allcats" style={{ textDecoration: 'none', color: 'white'}}>
-                        <Button color="inherit" size="large" sx={{textTransform: 'none'}}>
+                    <Link to="/allcats" style={{textDecoration: 'none', color: 'white'}}>
+                        <Button
+                            color="inherit"
+                            size="large"
+                            sx={{
+                                textTransform: 'none',
+                                backgroundColor: location.pathname === '/allcats' ? '#1E88E5' : 'transparent',
+                                '&:hover': {
+                                    backgroundColor: location.pathname === '/allcats' ? '#1E88E5' : 'rgba(255, 255, 255, 0.08)',
+                                },
+                            }}
+                        >
                             Все котики
                         </Button>
                     </Link>
-                    <Link to="/lovelycats" style={{ textDecoration: 'none', color: 'white'}}>
-                        <Button color="inherit" size="large" sx={{textTransform: 'none'}}>
+                    <Link to="/lovelycats" style={{textDecoration: 'none', color: 'white'}}>
+                        <Button
+                            color="inherit"
+                            size="large"
+                            sx={{
+                                textTransform: 'none',
+                                backgroundColor: location.pathname === '/lovelycats' ? '#1E88E5' : 'transparent',
+                                '&:hover': {
+                                    backgroundColor: location.pathname === '/lovelycats' ? '#1E88E5' : 'rgba(255, 255, 255, 0.08)',
+                                },
+                            }}
+                        >
                             Любимые котики
                         </Button>
                     </Link>
@@ -27,4 +47,4 @@ function Header() {
     );
 }
 
-export default Header
+export default Header;
