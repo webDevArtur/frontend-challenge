bbf34b68f7c683eb4f85298e7f5c7510bf116a54
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2024-01-30 04:29:03 +0300

    Update LovelyCatsPage.tsx

--- src/pages/LovelyCatsPage/LovelyCatsPage.tsx
+++ src/pages/LovelyCatsPage/LovelyCatsPage.tsx
@@ -1,6 +1,6 @@
-import { observer } from 'mobx-react-lite';
-import { Grid, Paper, IconButton } from '@mui/material';
-import { Favorite, FavoriteBorder } from '@mui/icons-material';
+import {observer} from 'mobx-react-lite';
+import {Grid, Paper, IconButton, Typography, Box} from '@mui/material';
+import {Favorite, FavoriteBorder} from '@mui/icons-material';
 import mobXStore from '../../store/CatStore';
 
 const LovelyCatsPage = observer(() => {
@@ -11,20 +11,30 @@ const LovelyCatsPage = observer(() => {
     };
 
     return (
-        <Grid container spacing={2} style={{ padding: 16 }}>
-            {likedCats.map((cat, index) => (
-                <Grid key={index} item xs={12} sm={6} md={4} lg={3} xl={2}>
-                    <Paper elevation={3} style={{ boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)', width: '255px', height: '255px', position: 'relative' }}>
-                        <img src={cat.url} alt={`Liked Kitten ${index}`} style={{ width: '255px', height: '255px' }} />
-                        <IconButton
-                            onClick={() => handleLikeToggle(cat.id)}
-                            style={{ position: 'absolute', bottom: 8, right: 8, color: 'red' }}
-                        >
-                            {cat.liked ? <Favorite /> : <FavoriteBorder />}
-                        </IconButton>
-                    </Paper>
-                </Grid>
-            ))}
+        <Grid container spacing={2} style={{padding: 16}}>
+            {likedCats.length > 0 ? (
+                likedCats.map((cat, index) => (
+                    <Grid key={index} item xs={12} sm={6} md={4} lg={3} xl={2}>
+                        <Paper elevation={3}
+                               style={{boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)', width: '255px', height: '255px', position: 'relative'}}>
+                            <img src={cat.url} alt={`Liked Kitten ${index}`} style={{width: '255px', height: '255px'}}/>
+                            <IconButton
+                                onClick={() => handleLikeToggle(cat.id)}
+                                style={{position: 'absolute', bottom: 8, right: 8, color: 'red'}}
+                            >
+                                {cat.liked ? <Favorite/> : <FavoriteBorder/>}
+                            </IconButton>
+                        </Paper>
+                    </Grid>
+                ))
+            ) : (
+                <Box
+                    sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100%'}}>
+                    <Typography variant="h5" align="center" color="textSecondary">
+                        У вас нет понравившихся изображений. Начните лайкать котиков!
+                    </Typography>
+                </Box>
+            )}
         </Grid>
     );
 });
